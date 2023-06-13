7a6d28faeabb444103b36a7970b4837616b52ae5
Haoqun Jiang <haoqunjiang@gmail.com>
2023-06-13 16:21:19 +0800

    version 3.7.1 snapshot

--- router-with-tests/vite.config.js
+++ router-with-tests/vite.config.js
@@ -2,13 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    nightwatchPlugin(),
   ],
   resolve: {
     alias: {
--- typescript-jsx-pinia-with-tests/vite.config.ts
+++ typescript-jsx-pinia-with-tests/vite.config.ts
@@ -3,14 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    nightwatchPlugin(),
   ],
   resolve: {
     alias: {
