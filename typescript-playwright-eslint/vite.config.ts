4d799281841826ab45db924d626765e0efb2b2f1
Haoqun Jiang <github@haoqun.me>
2026-06-17 23:57:44 +0900

    version 3.22.4 snapshot

--- typescript-playwright-eslint/vite.config.ts
+++ typescript-playwright-eslint/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
